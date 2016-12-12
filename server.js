d3d0ccbe9c472053b6296db8b221bda63d2c5f7c
tobby2002 <tobby2002@gmail.com>
2016-12-13 00:56:11 +0900

    flogin social fb login

--- server.js
+++ server.js
@@ -2,8 +2,8 @@
 
 'use strict';
 
-var server = require('./app_mr');
-var port = process.env.PORT || process.env.VCAP_APP_PORT || 80;
+var server = require('./app');
+var port = process.env.PORT || process.env.VCAP_APP_PORT || 3030;
 
 server.listen(port, function() {
   console.log('Server running on port: %d', port);
